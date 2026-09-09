dc87e39ac3d4cfc9bd1cd15bc79f488aac0ea8aa
kanchan <bishtkanchan161@gmail.com>
2026-09-09 22:44:58 +0530

    Modified expense module

--- expense.py
+++ expense.py
@@ -10,13 +10,8 @@ class Expense:
         print("Amount:", self.amount)
         print("Category:", self.category)
         print("Description:", self.description)
+       
 
 
-date = input("Enter the Date: ")
-amount = float(input("Enter the Amount: "))
-category = input("Enter the Category: ")
-description = input("Enter the Description: ")
 
-exp = Expense(date, amount, category, description)
 
-exp.display_expenses()
