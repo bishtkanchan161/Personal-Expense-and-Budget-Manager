b4a824c4cba42a412384f0262c05f42b09ab3f6c
kanchan <bishtkanchan161@gmail.com>
2026-09-15 22:26:26 +0530

    Updated expenses analysis using pandas

--- budget.py
+++ budget.py
@@ -1,5 +1,5 @@
 from expense import Expense
-import numpy as np
+
 import pandas as pd
 class Budget:
     def __init__(self, income=0, budget=0):
@@ -19,13 +19,27 @@ class Budget:
         
     def set_budget(self):
         budget=float(input("enter your budget: "))
+        
         self.budget=budget
         print("Your total budget is:",self.budget)
 
     def update_budget(self):
-            new_budget= float(input("enter your  new monthly budget:"))
-            self.new_budget=new_budget
-            print("Your new total budget is:",self.new_budget)
+        new_budget= float(input("enter your  new monthly budget:"))
+        self.new_budget=new_budget
+        print("Your new total budget is:",self.new_budget)
+    def create_dataframe(self):
+        data=[]
+        for exp in self.expenses:
+            data.append({
+                "Date":exp.date,
+                "Amount":exp.amount,
+                "Category":exp.category,
+                "Description":exp.description
+            })
+        df=pd.DataFrame(data)
+        return df
+    
+         
        
     def menu(self):
         while True:
@@ -36,9 +50,11 @@ class Budget:
         4. Press 4 for update monthly budget
         5. Press 5 for add expenses
         6. Press 6 for show all expenses Details
+        7. Press 7 for show expenses analysis
+        8. Press 8 for show remainig budget
         
        
-        6. Press 7 for exist
+        9. Press 9 for exist
         ''' )
             print("Your choice is:", user_input)
             if user_input=="1":
@@ -62,17 +78,29 @@ class Budget:
                 
                 exp.display_expenses()
             elif user_input=="6":
-                data=[]
-                for exp in self.expenses:
-                     data.append({
-                     "Date":exp.date,
-                     "Amount":exp.amount,
-                     "Category":exp.category,
-                     "Description":exp.description
-                })
-                df=pd.DataFrame(data)
-            
+                df=self.create_dataframe()
                 print(df)
+            
+            elif user_input=="7":
+                df=self.create_dataframe()
+                total_expenses=df["Amount"].sum()
+                print("Total Expenses is:",total_expenses)
+
+                average_expenses=df["Amount"].mean()
+                print("Average of Expenses is:",average_expenses)
+
+                max_expense=df["Amount"].max()
+                print("Maximum Expense is:",max_expense)
+
+                min_expense=df["Amount"].min()
+                print("Minimum Expense is:",min_expense)
+
+
+            elif user_input=="8":
+                df=self.create_dataframe()
+                rem_budget=self.budget-total_expenses
+                print("Remainig Budget is:",rem_budget)
+
                
             
            
